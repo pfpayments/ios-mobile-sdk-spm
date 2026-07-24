d28ed23b3a16514b41404039ea86b3c40266c835
wallee-deployment-user <technical-systems@wallee.com>
2026-07-24 14:01:37 +0200

    2.0.0

--- Package.swift
+++ Package.swift
@@ -5,23 +5,17 @@ let package = Package(
     name: "PostFinanceCheckoutSdk",
     platforms: [.iOS("12.4")],
     products: [
-        .library(name: "PostFinanceCheckoutSdk",
-                 targets: ["ThreeDS_SDK","TwintSDK","PostFinanceCheckoutSdk","PaymentResources"]),
+        .library(name: "PostFinanceCheckoutSdk", targets: ["PostFinanceCheckoutSdk"]),
+        .library(name: "ReactBrownfield", targets: ["ReactBrownfield"]),
+        .library(name: "ThreeDS_SDK", targets: ["ThreeDS_SDK"]),
+        .library(name: "TwintSDK", targets: ["TwintSDK"]),
+        .library(name: "hermes", targets: ["hermes"])
     ],
     targets: [
-		.binaryTarget(name: "ThreeDS_SDK", path: "ThreeDS_SDK.xcframework"),
-		.binaryTarget(name: "TwintSDK", path: "TwintSDK.xcframework"),
-		.binaryTarget(name: "PostFinanceCheckoutSdk", path: "PostFinanceCheckoutSdk.xcframework"),
-	.target(
-		name: "PaymentResources",
-		dependencies: [
-			.target(name: "PostFinanceCheckoutSdk")
-		],
-		path: "Sources/PaymentResources",
-		sources: ["PaymentResources.swift"],
-		resources: [
-			.process("postfinancecheckoutsdkbundle.jsbundle")
-		]
-	)
+        .binaryTarget(name: "PostFinanceCheckoutSdk", path: "./PostFinanceCheckoutSdk.xcframework"),
+        .binaryTarget(name: "ReactBrownfield", path: "./ReactBrownfield.xcframework"),
+        .binaryTarget(name: "ThreeDS_SDK", path: "./ThreeDS_SDK.xcframework"),
+        .binaryTarget(name: "TwintSDK", path: "./TwintSDK.xcframework"),
+        .binaryTarget(name: "hermes", path: "./hermes.xcframework")
     ]
 )
--- PostFinanceCheckoutSdk.xcframework/ios-arm64_x86_64-simulator/PostFinanceCheckoutSdk.framework/Headers/PostFinanceCheckoutSdk.h
+++ PostFinanceCheckoutSdk.xcframework/ios-arm64_x86_64-simulator/PostFinanceCheckoutSdk.framework/Headers/PostFinanceCheckoutSdk.h
@@ -1,11 +1,9 @@
 #import <Foundation/Foundation.h>
 
-
 //! Project version number for PostFinanceCheckoutSdk.
 FOUNDATION_EXPORT double PostFinanceCheckoutSdkVersionNumber;
 
 //! Project version string for PostFinanceCheckoutSdk.
 FOUNDATION_EXPORT const unsigned char PostFinanceCheckoutSdkVersionString[];
 
-
-// In this header, you should import all the public headers of your framework using statements like #import <PostFinanceCheckoutSdk/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <WalleePaymentSdk/PublicHeader.h>
